24936c67e40a15566aeb9592d88e1918332f86ac
Amit Merchant <bullredeyes@gmail.com>
2016-03-11 13:58:22 +0530

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pretty Email Validator
+> Pretty validation for emails
+
+This is a small node module which checks for the validity of emails.
---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function prettyValidateEmail(email) {
+
+    if(!email)
+      return false;
+
+    if(email.length>254)
+      return false;
+
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+
+}
+
+module.exports = validateEmail;
